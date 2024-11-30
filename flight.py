8ea82dedec35771e6a049c1dc538ed13adde82dc
plezerm <153388968+plezerm@users.noreply.github.com>
2024-11-30 18:50:01 +0100

    Járat osztályok

--- flight.py
+++ flight.py
@@ -2,10 +2,31 @@ from abc import ABC, abstractmethod
 
 # Ős repülőjárat
 
+class Flight(ABC):
+    def __init__(self, flight_number, destination, price):
+        self.flight_number = flight_number
+        self.destination = destination
+        self.price = price
 
+    @abstractmethod
+    def __str__(self):
+        pass
 
 # Belföldi Járat
 
+class DomesticFlight(Flight):
+    def __init__(self, flight_number, destination, price):
+        super().__init__(flight_number, destination, price)
+
+    def __str__(self):
+        return f"Domestic flight {self.flight_number}: {self.destination} - {self.price} Ft"
 
 
 # Nemzetközi Járat
+
+class InternationalFlight(Flight):
+    def __init__(self, flight_number, destination, price):
+        super().__init__(flight_number, destination, price)
+
+    def __str__(self):
+        return f"International flight {self.flight_number}: {self.destination} - {self.price} Ft"
